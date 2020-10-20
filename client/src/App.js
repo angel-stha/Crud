39248c255997542f93622e679289a701bc0d48f5
Angel Shrestha <angelshrestha33@gmail.com>
2020-10-20 16:18:10 +0545

    delete and get changed

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
 
 
   useEffect(()=> {
-    Axios.get('http:\\localhost:30001/api/get').then((response) => {
+    Axios.get('http://localhost:3001/api/get').then((response) => {
       setMovieList(response.data);
     });
   }, []);
@@ -29,7 +29,11 @@ function App() {
 
   
   const deleteReview =(movie) => {
-    Axios.post('http://localhost:3001/api/delete/${movie}');
+      console.log(movie)
+
+    Axios.post('http://localhost:3001/api/delete/',{
+        movieName:movie,
+    });
 
   }
 
